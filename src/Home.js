66837d99fc16af1a07d563f69cd5adfbb4b55a2c
Charley <charlzahk@gmail.com>
2019-12-18 17:04:59 -0500

    Add Carousel (rough)

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,60 @@
-import React, { Component } from 'react'
-import Carousel from 'react-bootstrap/Carousel';
-
+import React, { Component } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import './Home.css'
 
 class Home extends Component {
   render() {
     return (
-    <div className="gallery">
-        <h6>
-        Home
-        </h6>
-        <Carousel> 
-
+      <div className="Home">
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={require(`./Banner1.jpeg`)}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={require(`./Banner2.jpeg`)}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={require(`./Banner3.jpeg`)}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={require(`./Banner4.gif`)}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
         </Carousel>
-    </div>
-
-    )
+      </div>
+    );
   }
-
 }
 
-export default Home
-
+export default Home;
