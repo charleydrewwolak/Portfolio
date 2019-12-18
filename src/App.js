9da25beed76ec04d178b386a5248f53b74781a2b
Charley <charlzahk@gmail.com>
2019-12-18 17:56:59 -0500

    Add about

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Contact from './Contact';
-import CV from './CV';
-import Gallery from './Gallery'
-import Home from './Home'
-import About from './About'
-import { Nav, Navbar, } from 'react-bootstrap';
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Contact from "./Contact";
+import CV from "./CV";
+import Gallery from "./Gallery";
+import Home from "./Home";
+import About from "./About";
+import { Nav, Navbar } from "react-bootstrap";
 
 function App() {
-  
   return (
     <div className="App">
       <Navbar bg="light" variant="light">
         <Navbar.Brand href="#">Charley Drew-Wolak</Navbar.Brand>
+
         <Nav className="mr-auto">
           <Nav.Link href="#cv">CV</Nav.Link>
           <Nav.Link href="#gallery">Gallery</Nav.Link>
@@ -22,15 +22,15 @@ function App() {
           <Nav.Link href="#about">About</Nav.Link>
         </Nav>
       </Navbar>
-      <Router basename='/'>
-        <Route exact path="/" component={Home} />
-        <Route path='/contact' component={Contact} />
-        <Route path='/cv' component={CV} />
-        <Route path='/gallery' component={Gallery} />
-        <Route path='/about' component={About} />
-      </Router>
-
-
+      <div className="Body">
+        <Router basename="/">
+          <Route exact path="/" component={Home} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/cv" component={CV} />
+          <Route path="/gallery" component={Gallery} />
+          <Route path="/about" component={About} />
+        </Router>
+      </div>
     </div>
   );
 }
