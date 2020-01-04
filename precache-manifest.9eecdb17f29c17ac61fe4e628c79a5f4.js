b6d80efa65e29743b56a39bae449c9eb0d17064b
Charley <charlzahk@gmail.com>
2020-01-03 21:01:25 -0500

    Updates

---
+++ precache-manifest.9eecdb17f29c17ac61fe4e628c79a5f4.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0523f64f8f51e34dcfe081867f597a0b",
+    "url": "charleydrewwolak.github.io/Portfolio/index.html"
+  },
+  {
+    "revision": "d7a34391f1e9b4c680f1",
+    "url": "charleydrewwolak.github.io/Portfolio/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f647a69d51ca2793b0ba",
+    "url": "charleydrewwolak.github.io/Portfolio/static/css/main.6a0626bf.chunk.css"
+  },
+  {
+    "revision": "d7a34391f1e9b4c680f1",
+    "url": "charleydrewwolak.github.io/Portfolio/static/js/2.81777374.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "charleydrewwolak.github.io/Portfolio/static/js/2.81777374.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f647a69d51ca2793b0ba",
+    "url": "charleydrewwolak.github.io/Portfolio/static/js/main.d1bc1b91.chunk.js"
+  },
+  {
+    "revision": "141a8e8856f773166f6b",
+    "url": "charleydrewwolak.github.io/Portfolio/static/js/runtime-main.8eb7637e.js"
+  },
+  {
+    "revision": "0682eec85031f68b0f6d41f919504e98",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Ajna1.0682eec8.jpeg"
+  },
+  {
+    "revision": "bd28cc0f8ebd6d92f2d5afb0f4d9bc36",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Ajna2.bd28cc0f.jpeg"
+  },
+  {
+    "revision": "a2d3c2a666ea0be96c9e433b5b8f6905",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/AjnaBroach1.a2d3c2a6.jpeg"
+  },
+  {
+    "revision": "3de8953f0b7dba70b8ed0740a11cd858",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/AjnaBroach2.3de8953f.jpg"
+  },
+  {
+    "revision": "5fed2e60fac16864ddd1b1c1707f23f3",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Alloy1.5fed2e60.png"
+  },
+  {
+    "revision": "e828763e88e6f0cee9cf099f7609bffe",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Alloy2.e828763e.png"
+  },
+  {
+    "revision": "ecf5c831d598b677661cd6bdcbca8276",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Banjo.ecf5c831.gif"
+  },
+  {
+    "revision": "d6d5dfe0aa0033bccffae1921072d565",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Banjo1.d6d5dfe0.jpg"
+  },
+  {
+    "revision": "74d0771255f73d96fafe9775ca2a6d1e",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Banner1.74d07712.jpeg"
+  },
+  {
+    "revision": "ee59911c8d119bbb33b8700896868cc3",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Banner2.ee59911c.jpeg"
+  },
+  {
+    "revision": "53e57e5db1af2653d4ea8c7ded1ad0c4",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Banner3.53e57e5d.jpeg"
+  },
+  {
+    "revision": "ecf5c831d598b677661cd6bdcbca8276",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Banner4.ecf5c831.gif"
+  },
+  {
+    "revision": "7edfc81a812ee92af15831d2af6b2a1f",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Butterfly.7edfc81a.jpg"
+  },
+  {
+    "revision": "a451b00ac54aeca3c5a9fd9eb30b4f5c",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/CastRing.a451b00a.jpg"
+  },
+  {
+    "revision": "d6edb349b340448beddfab1a7b0477df",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Caverns.d6edb349.jpg"
+  },
+  {
+    "revision": "d42b4651de2a3245cb6316d1f133ac80",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Chair 3.d42b4651.3dm"
+  },
+  {
+    "revision": "50dd7ad9564b78341cd87c350391d92d",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Earth.50dd7ad9.jpg"
+  },
+  {
+    "revision": "ad2bec958b78156be28dd0ed058fc37e",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Earth2.ad2bec95.jpg"
+  },
+  {
+    "revision": "969e3185669d681f195776cff7cf9138",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/EverydayShelf.969e3185.jpeg"
+  },
+  {
+    "revision": "2b42762480e450cf4a50f95b11dee1ca",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Flux.2b427624.jpg"
+  },
+  {
+    "revision": "ed7042e6f49f835458f5cfe1b56c3a13",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Flux1.ed7042e6.jpg"
+  },
+  {
+    "revision": "cad9b7d58f9e24a5210406ca6c078919",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Flux2.cad9b7d5.jpg"
+  },
+  {
+    "revision": "dadac06ba42a2a446faf184b1a073592",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/GayWatteringcan1.dadac06b.jpeg"
+  },
+  {
+    "revision": "c7523ce1e61a17065ed1e05c08020652",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Headshot.c7523ce1.jpeg"
+  },
+  {
+    "revision": "51f0c63d580d7b7cc31564d09d94144e",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/HollowformRing.51f0c63d.jpg"
+  },
+  {
+    "revision": "f9a21d2e4afce56031573ce0676547f0",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/HollowformRing2.f9a21d2e.jpg"
+  },
+  {
+    "revision": "5574af89d65ca311b4305a2982a6688b",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/IMG_0624.5574af89.mp4"
+  },
+  {
+    "revision": "4409b101cfa6097e34721f718cd6de80",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/InOurHands.4409b101.mp4"
+  },
+  {
+    "revision": "fdce845cecdc82ffc64f87906aa15884",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/InOurHands.fdce845c.jpg"
+  },
+  {
+    "revision": "8064919833aeece2424ab74d796198db",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Light1.80649198.jpg"
+  },
+  {
+    "revision": "ab90944e9f4ea7c6035bfb82ad774eca",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Light2.ab90944e.jpg"
+  },
+  {
+    "revision": "ca511db77d7a61345eb79accf8f3b37d",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Logo.ca511db7.svg"
+  },
+  {
+    "revision": "4cccb1af62e050cd4394cdaa9b435be2",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/MetrosexualVase1.4cccb1af.jpeg"
+  },
+  {
+    "revision": "6398cd3d2df3f8f7568514be8506b087",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/MetrosexualVase2.6398cd3d.jpeg"
+  },
+  {
+    "revision": "904132232b12043c4d09357ed86806c9",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/OilFree.90413223.png"
+  },
+  {
+    "revision": "56bc8ce0a7e61cc9626171e82a31a9da",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Planets.56bc8ce0.jpg"
+  },
+  {
+    "revision": "1bab8e2602d6d071ba48483b76aab553",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/RhinoBowl.1bab8e26.jpg"
+  },
+  {
+    "revision": "96ea50d05c57701b79214c3090d0a6db",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Symbol.96ea50d0.jpeg"
+  },
+  {
+    "revision": "84dfa5977070cd62073556607862b218",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Vascular1.84dfa597.jpg"
+  },
+  {
+    "revision": "3192814f7a49b02423f219f2ed7fb236",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Vascular2.3192814f.jpg"
+  },
+  {
+    "revision": "fc2bdd472a9d8383335431a24813124b",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Vascular3.fc2bdd47.jpg"
+  },
+  {
+    "revision": "fa3de40321118eaa3fb97ea5c2331d02",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Vascular4.fa3de403.jpg"
+  },
+  {
+    "revision": "715a243c03b5c7accd335a1e30ed5f8f",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Vascular5.715a243c.jpg"
+  },
+  {
+    "revision": "9606836c7ec281378c931417a0eeff17",
+    "url": "charleydrewwolak.github.io/Portfolio/static/media/Vascular6.9606836c.jpeg"
+  }
+]);
