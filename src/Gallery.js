a8df13030b76fb71c2b790c86aecf0bc626a0e56
Charley <charlzahk@gmail.com>
2020-01-07 15:51:11 -0500

    Updates- More gallery, typo fixes

--- src/Gallery.js
+++ src/Gallery.js
@@ -13,42 +13,75 @@ class Gallery extends Component {
           files: ["Vascular1.jpg", "Vascular2.jpg", "Vascular3.jpg"],
           materials: "Steel",
           year: "2019",
-          shows: "Strange Company, Penald NC",
+          shows: "Strange Company, Penland NC",
           info:
-            "To be human is to make. Craft was once an essential skill for survival. Humans evolved by being creative and resourceful. We deskill ourselves as we continue to grow into post-industrial society, leaving behind our traditions and our very nature. Today we exist beyond what we’ve known to be natural. We now possess technologies that can rewrite our very own DNA. Vascular​ is a part of a series that will question what it means to exist in a post-human world. Technology is a part of us, woven into the fabric of our society. I will use Arduino controlled sensors and lighting combined with wearable sculpture to create a series that dynamically responds with the body. This piece is half done. The steal vein structure engulfs and explodes from the body. Red glowing EL wire will follow the structure, pulsing to the wearer’s heart rate. The explosive vein nature expresses how our relationship with technology can be both symbiotic and parasitic."
+            "To be human is to make. Craft was once an essential skill for survival. Humans evolved by being creative and resourceful. We deskill ourselves as we continue to grow into post-industrial society, leaving behind our traditions and our very nature. Today we exist beyond what we’ve known to be natural. We now possess technologies that can rewrite our very own DNA. Vascular​ is a part of a series that will question what it means to exist in a post-human world. Technology is a part of us, woven into the fabric of our society. I will use Arduino controlled sensors and lighting combined with wearable sculpture to create a series that dynamically responds with the body. This piece is half done. The steel vein structure engulfs and explodes from the body. Red glowing EL wire will follow the structure, pulsing to the wearer’s heart rate. The explosive vein nature expresses how our relationship with technology can be both symbiotic and parasitic."
         },
         {
           title: "Ajna Broach",
           files: ["AjnaBroach1.jpeg", "AjnaBroach2.jpg"],
-          materials: "Steal, Silver",
-          shows: "Strange Company, Penald NC",
+          materials: "Steel, Silver",
+          shows: "Strange Company, Penland NC",
           year: "2019",
           info: ""
         },
         {
           title: "Ajna",
           files: ["Ajna1.jpeg", "Ajna2.jpeg"],
-          materials: "Steal",
-          shows: "Strange Company, Penald NC",
+          materials: "Steel",
+          shows: "Strange Company, Penland NC",
           year: "2019",
           info: ""
         },
         {
           title: "Gay Wattering Can",
           files: ["GayWatteringcan1.jpeg"],
-          materials: "Steal, Spraypaint, Glitter",
+          materials: "Steel, Spraypaint, Glitter",
           year: "2019",
-          shows: "Strange Company, Penald NC",
+          shows: "Strange Company, Penland NC",
           info: ""
         },
         {
           title: "Metrosexual Vase",
           files: ["MetrosexualVase1.jpeg", "MetrosexualVase2.jpeg"],
-          materials: "Steal, Spraypaint",
+          materials: "Steel, Spraypaint",
           year: "2019",
-          shows: "Strange Company, Penald NC",
+          shows: "Strange Company, Penland NC",
           info: ""
-        }
+        },
+        {
+          title: "Inner Light",
+          files: ["Light1.jpg", "Light2.jpg"],
+          materials: "Silver, Enamel, Steel",
+          year: "2019",
+          shows: "",
+          info: "Sgraffito and Plique-à-Jour enameling techniques were used to make this broach inspired by Waldorf gnome lore - specifically a story of Puck the Gnome. Holding the broach up to the light reveals the glowing crystal that guided Puck on his journey through the depths of the earth. The broach is to be worn on the heart as a reminder to let your inner light guide you in your journey through life."
+        },
+        {
+          title: "Caverns",
+          files: ["Caverns.jpg"],
+          materials: "Silver, Copper, Enamel",
+          year: "2019",
+          shows: "",
+          info: "Sgraffito enameling techniques were used to make this necklace to correspond to the Inner Light Broach. When worn, the cavers open at the heart. This piece reflects the journey through darkness to reach the clarity that comes with being true to your heart."
+        },
+        {
+          title: "In Our Hands",
+          files: ["InOurHands.jpg"],
+          materials: "Copper, Powder Coat, PLA, Spray Paint, Wood",
+          year: "2018",
+          shows: "",
+          info: ""
+        },
+        {
+          title: "Life of the Mountains",
+          files: ["Banjo.gif", "Banjo1.jpg", "Banjo2.jpg"],
+          materials: "Copper, Enamel, Acrylic, Wood",
+          year: "2018",
+          shows: "",
+          info: "My intent with this piece was symbolize how music can serve as a window. Appalachian music brings a community together to dance, sing, and celebrate life. It also expresses and preserves a history of hardship, exploitation, resistance, and resilience. On one side of Pine Mountain you see breath taking views of rolling green mountains as you drive down winding roads to Cowan Creek. Drive down the opposite direction and you see a void of dirt and destruction: nature strip mined away, layer by layer."
+        },
+
       ],
       GraphicDesign: [
         {
@@ -64,7 +97,7 @@ class Gallery extends Component {
         {
           title: "Louisville Bowl",
           files: ["RhinoBowl.jpg"],
-          materials: "Steal, Spraypaint, Glitter",
+          materials: "Steel, Spraypaint, Glitter",
           year: "2016",
           shows: "",
           info: "Inspired by the Mercer building's architecture.  "
@@ -72,13 +105,71 @@ class Gallery extends Component {
       ],
       Photography: [
         {
-          title: "Oil Free",
-          files: ["OilFree.png"],
-          materials: "Soap, Oil, Developed Print",
+          title: "Self Portrait 1",
+          files: ["Self3.jpeg"],
+          materials: "Scanned Print",
+          year: "2018",
+          shows: "",
+          info: "8x10"
+        },
+        {
+          title: "Self Portrait 2",
+          files: ["Self2.jpeg"],
+          materials: "Scanned Print",
+          year: "2018",
+          shows: "",
+          info: "8x10"
+        },
+        {
+          title: "Self Portrait 3",
+          files: ["Self4.jpeg"],
+          materials: "Scanned Print",
           year: "2018",
           shows: "",
           info: ""
         },
+        {
+          title: "",
+          files: ["O1.jpeg"],
+          materials: "Scanned Print",
+          year: "2018",
+          shows: "",
+          info: "8x10"
+        },
+        {
+          title: "",
+          files: ["O2.jpeg"],
+          materials: "Scanned Print",
+          year: "2018",
+          shows: "",
+          info: "8x10"
+        },
+        {
+          title: "",
+          files: ["O3.jpeg"],
+          materials: "Scanned Print",
+          year: "2018",
+          shows: "",
+          info: "8x10"
+        },
+        {
+          title: "",
+          files: ["O5.jpeg"],
+          materials: "Scanned Print",
+          year: "2018",
+          shows: "",
+          info: "8x10"
+        },
+        {
+          title: "",
+          files: ["O6.jpeg"],
+          materials: "Scanned Print",
+          year: "2018",
+          shows: "",
+          info: "8x10"
+        },
+        
+   
       ],
 
     };
